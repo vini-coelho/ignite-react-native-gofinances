7a9f21c1e93775215d0e91af15cf3cff4edb44bd
Vinicius Coelho <vinicius_coelho@live.com>
2021-07-12 12:51:04 -0400

    feat: add Dashboard screen

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/50638499?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Vinicius</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name='power'/>
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
+}
