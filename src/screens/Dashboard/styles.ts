9f8c82d9236fb7fc45108f9ed2a8b497780ef884
Vinicius Coelho <vinicius_coelho@live.com>
2021-07-12 13:18:15 -0400

    feat: add HighlightCard Component

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -14,7 +15,8 @@ export const Header = styled.View`
 
   background-color: ${({ theme }) => theme.colors.primary};
 
-  align-items: center;
+  flex-direction: row;
+  align-items: flex-start;
   justify-content: center;
 `;
 
@@ -22,6 +24,7 @@ export const UserWrapper = styled.View`
   width: 100%;
 
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)};
 
   flex-direction: row;
   justify-content: space-between;
@@ -61,4 +64,14 @@ export const UserName = styled.Text`
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
