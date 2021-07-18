a2eb1e59c812e9fdbf24a45b346bfa863663bba1
Vinicius Coelho <vinicius_coelho@live.com>
2021-07-18 16:33:17 -0400

    feat: adds google oauth2 logic

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,10 @@
-import { useContext } from "hoist-non-react-statics/node_modules/@types/react";
-import React, { createContext, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  ReactNode
+} from "react";
+
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -14,6 +19,7 @@ interface User {
 
 interface IAuthContextData {
   user: User;
+  signInWithGoogle: () => Promise<void>;
 }
 
 const AuthContext = createContext({} as IAuthContextData);
@@ -25,8 +31,25 @@ export function AuthProvider({ children }: AuthProviderProps) {
     email: 'vinicoelho@live.com'
   }
 
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID = '942050736281-ucr5mkgn2fj1d8tr15mdbbsea7qq9ieo.apps.googleusercontent.com';
+      const REDIRECT_URI = 'https://auth.expo.io/@vini-coelho/gofinances';
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('profile email');
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const response = await AuthSession.startAsync({ authUrl });
+
+      console.log(response);
+    } catch(err) {
+      throw new Error(err);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
       { children }
     </AuthContext.Provider>
   );
