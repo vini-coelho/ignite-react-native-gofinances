c06f21c32c42b424ab147b028dfa3ff273c7da39
Vinicius Coelho <vinicius_coelho@live.com>
2021-07-18 23:54:42 -0400

    feat: finish Google authentication

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -3,12 +3,12 @@ import React, {
   useContext,
   useState,
   ReactNode
-} from "react";
+} from 'react';
 
 const { CLIENT_ID } = process.env;
 const { REDIRECT_URI } = process.env;
 
-import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -45,7 +45,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const { type, params } = await AuthSession
+      const { type, params } = await Google
       .startAsync({ authUrl }) as AuthorizationResponse;
 
       if (type === 'success') {
